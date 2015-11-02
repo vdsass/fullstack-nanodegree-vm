572b92a56bb36d37510d301af0f83c21a5d2d525
vdsass <swdeveloper@cox.net>
2015-11-01 21:42:34 -0800

    changed 'matches' table

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -21,35 +21,50 @@ CREATE TABLE IF NOT exists players(
                                     pid serial primary key,
                                     name text
                                    );
-
+INSERT INTO players (pid, name) values(0,'Nobody');
 INSERT INTO players (name) values('Billy Bob Thorton');
 INSERT INTO players (name) values('Christian Bale');
 INSERT INTO players (name) values('Johnny Depp');
 INSERT INTO players (name) values('Brad Pitt');
+INSERT INTO players (name) values('Jon Ham');
 
-
+/*
 CREATE TABLE IF NOT exists matches (
     pid_1 integer REFERENCES players (pid),
     pid_2 integer REFERENCES players (pid),
     win_id integer REFERENCES players (pid)
 );
-
--- 1 2 3 4  W
--- x x      2
--- x   x    1
--- x     x  4
---   x x    2
---   x   x  4
---     x x  4
-
+--   MATCHES
+-- 1 2 3 4 5  W
+-- x x        2
+-- x   x      1
+-- x     x    4
+--   x x      2
+--   x   x    4
+--     x x    4
+--         X
 -- INSERT INTO matches(pid_1, pid_2, win_id) values();
 INSERT INTO matches (pid_1, pid_2, win_id) VALUES
+    (0, 0, 0),
     (1, 2, 2),
     (1, 3, 1),
     (1, 4, 4),
     (2, 3, 2),
     (2, 4, 4),
-    (3, 4, 4);
+    (3, 4, 4),
+    (4, 0, 0),
+    (5, 0, 0);
+*/
 
+CREATE TABLE IF NOT exists matches (
+    winner integer REFERENCES players (pid),
+    loser integer REFERENCES players (pid)
+);
 
+INSERT INTO matches (winner, loser) values(1,5);
+INSERT INTO matches (winner, loser) values(4,2);
+INSERT INTO matches (winner, loser) values(2,1);
+INSERT INTO matches (winner, loser) values(5,4);
+INSERT INTO matches (winner, loser) values(5,2);
+INSERT INTO matches (winner, loser) values(5,1);
 
